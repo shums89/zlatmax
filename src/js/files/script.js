8af51a5e284d7aaf554296c432ba91a112428bcb
Александр <sh889@ya.ru>
2022-01-06 00:54:23 +0500

    Верстка меню каталога в header

--- src/js/files/script.js
+++ src/js/files/script.js
@@ -4,3 +4,36 @@ import { isMobile } from "./functions.js";
 import { flsModules } from "./modules.js";
 
 
+document.addEventListener("click", documentActions);
+
+const menuBlocks = document.querySelectorAll('.sub-menu-catalog__block');
+if (menuBlocks.length) {
+  menuBlocks.forEach(menuBlock => {
+    const menuBlockItems = menuBlock.querySelectorAll('.sub-menu-catalog__category').length;
+    menuBlock.classList.add(`sub-menu-catalog__block_${menuBlockItems}`);
+  });
+}
+
+function documentActions(e) {
+  const targetElement = e.target;
+
+  if (targetElement.closest('[data-parent]')) {
+    const subMenuId = targetElement.dataset.parent ? targetElement.dataset.parent : null;
+    const subMenu = document.querySelector(`[data-submenu="${subMenuId}"]`);
+
+    if (subMenu) {
+      const activeLink = document.querySelector('._sub-menu-active');
+      const activeBlock = document.querySelector('._sub-menu-open');
+
+      if (activeLink && activeLink !== targetElement) {
+        activeLink.classList.remove('_sub-menu-active');
+        activeBlock.classList.remove('_sub-menu-open');
+      }
+      targetElement.classList.toggle('_sub-menu-active');
+      subMenu.classList.toggle('_sub-menu-open');
+    } else {
+      console.log('Ой ой, нет такого подменю');
+    }
+    e.preventDefault();
+  }
+}
